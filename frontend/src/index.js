72a58789aba2fd78e7809da6eae6aaec0eafaf32
Ezeoye Goodluck <Ezeoyeg@gmail.com>
2023-08-08 23:08:58 +0100

    commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,7 @@ import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 import { Provider } from 'react-redux';
 import  store from './store';
 
@@ -22,6 +23,7 @@ const router = createBrowserRouter(
       <Route index={true} path='/product/:id' element={<ProductScreen />} />
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />}></Route>
+      <Route path='/register' element={<RegisterScreen/> } />
     </Route>
   )
 )
