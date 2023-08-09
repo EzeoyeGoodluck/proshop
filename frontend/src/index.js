994f3bb7e9d7f865ed04cc08f03aff09585f0970
Ezeoye Goodluck <Ezeoyeg@gmail.com>
2023-08-09 22:37:17 +0100

    commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -16,6 +16,7 @@ import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import { Provider } from 'react-redux';
 import  store from './store';
+import PrivateRoute from './components/PrivateRoute';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -25,7 +26,12 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />}></Route>
       <Route path='/register' element={<RegisterScreen/> } />
+
+      <Route path='' element={<PrivateRoute/> }>
+
       <Route path='/shipping' element={<ShippingScreen />} />
+
+      </Route>
     </Route>
   )
 )
