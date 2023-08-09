9a6abecdf6ef5ea92921e1de4445bb4b57992b94
Ezeoye Goodluck <Ezeoyeg@gmail.com>
2023-08-09 22:26:27 +0100

    commit

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -2,16 +2,32 @@ import { useState } from "react";
 import {Form, Button } from 'react-bootstrap';
 import FormContainer from "../components/FormContainer";
 
+import { useDispatch, useSelector } from "react-redux";
+import {useNavigate } from "react-router-dom";
+import { saveShippingAddress } from "../slices/cartSlice";
 
 const ShippingScreen = () => {
-    const [address, setAddress] = useState('');
-    const [city, setCity] = useState('');
-    const [postaCode, setPostaCode] = useState('');
-    const[country, setCountry] = useState('');
 
 
-    const submitHandler = () =>{
-        console.log('yes')
+    const cart = useSelector((state) => state.cart);
+    const { shippingAddress } = cart;
+
+    const [address, setAddress] = useState(shippingAddress?.address || '');
+    const [city, setCity] = useState( shippingAddress?.city || '');
+    const [postalCode, setPostalCode] = useState( shippingAddress?.postalCode || '');
+    const[country, setCountry] = useState( shippingAddress?.country || '');
+
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+ 
+
+    const submitHandler = (e) =>{
+        e.preventDefault();
+        dispatch(saveShippingAddress({ address, city, postalCode, country}));
+        navigate('/payment');
+
     }
   return (
     <FormContainer>
@@ -24,10 +40,47 @@ const ShippingScreen = () => {
                 placeholder="Enter address"
                 value={address}
                 onChange={(e) => setAddress(e.target.value)}
-                >Hello
+                >
                 </Form.Control>
-
             </Form.Group>
+
+            <Form.Group controlled='city' className="my-2">
+                <Form.Label>City</Form.Label>
+                <Form.Control
+                type='text'
+                placeholder="Enter city"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+                >
+                </Form.Control>
+            </Form.Group>
+
+            <Form.Group controlled='postalCode' className="my-2">
+                <Form.Label>Postal Code</Form.Label>
+                <Form.Control
+                type='text'
+                placeholder="Enter Postal Code"
+                value={postalCode}
+                onChange={(e) => setPostalCode(e.target.value)}
+                >
+                </Form.Control>
+            </Form.Group>
+
+            
+            <Form.Group controlled='country' className="my-2">
+                <Form.Label>Country</Form.Label>
+                <Form.Control
+                type='text'
+                placeholder="Enter Country"
+                value={country}
+                onChange={(e) => setCountry(e.target.value)}
+                >
+                </Form.Control>
+            </Form.Group>
+
+            <Button type="submit" variant="primary" className="my-2">
+                Continue
+            </Button>
         </Form>
     </FormContainer>
   )
