03d3ad1cacef9e4bae6e0b132e27e060634d068c
Ezeoye Goodluck <Ezeoyeg@gmail.com>
2023-08-09 15:49:48 +0100

    commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,6 +13,7 @@ import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
 import { Provider } from 'react-redux';
 import  store from './store';
 
@@ -24,6 +25,7 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />}></Route>
       <Route path='/register' element={<RegisterScreen/> } />
+      <Route path='/shipping' element={<ShippingScreen />} />
     </Route>
   )
 )
@@ -36,4 +38,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
+import {Form, Button } from 'react-bootstrap';
+import FormContainer from "../components/FormContainer";
+
 
 const ShippingScreen = () => {
+    const [address, setAddress] = useState('');
+    const [city, setCity] = useState('');
+    const [postaCode, setPostaCode] = useState('');
+    const[country, setCountry] = useState('');
+
+
+    const submitHandler = () =>{
+        console.log('yes')
+    }
   return (
-    <div>
-        
-    </div>
+    <FormContainer>
+        <h1>Shipping</h1>
+        <Form onSubmit={submitHandler}>
+            <Form.Group controlled='address' className="my-2">
+                <Form.Label>Address</Form.Label>
+                <Form.Control
+                type='text'
+                placeholder="Enter address"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                >Hello
+                </Form.Control>
+
+            </Form.Group>
+        </Form>
+    </FormContainer>
   )
 }
 
