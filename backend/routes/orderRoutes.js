0f9b45adcce90bcb206eeb74a8eeb924dda61dd2
Ezeoye Goodluck <Ezeoyeg@gmail.com>
2023-08-14 22:32:48 +0100

    commit

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -10,7 +10,8 @@ import {
 } from "../controllers/orderController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
-router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
+// router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
+router.route("/").post(protect, addOrderItems).get(protect, getOrders);
 router.route('/mine').get(protect, getMyOrders);
 router.route('/:id').get(protect, admin, getOrderById);
 router.route('/:id/pay').put(protect, admin,
