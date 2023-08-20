fc2c184225131f859ee83f074f1eb7f0a3fc1a6a
Ezeoye Goodluck <Ezeoyeg@gmail.com>
2023-08-20 22:25:24 +0100

    commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -21,6 +21,7 @@ import  store from './store';
 import PrivateRoute from './components/PrivateRoute';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 
 const router = createBrowserRouter(
@@ -38,6 +39,7 @@ const router = createBrowserRouter(
       <Route path='/payment' element={<PaymentScreen />} />
       <Route path='/placeorder' element={<PlaceOrderScreen/>} />
       <Route path='/order/:id' element={<OrderScreen /> }  />
+      <Route path='/profile' element={<ProfileScreen />}  />
       </Route>
     </Route>
   )
--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ProfileScreen = () => {
   return (
     <div>
-      
+        <p>Profile Screen</p>
     </div>
   )
 }
