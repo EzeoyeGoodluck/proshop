44948d42311fd74ff8a9b4c9cc303d83e0b49374
Ezeoye Goodluck <Ezeoyeg@gmail.com>
2023-08-25 12:14:20 +0100

    commit

--- frontend/src/screens/admin/OrderListScreen.jsx
+++ frontend/src/screens/admin/OrderListScreen.jsx
@@ -13,10 +13,10 @@ const OrderListScreen = () => {
     <>
       <h1>Orders</h1>
 
-      {/* {isLoading ? (
+      {isLoading ? (
         <Loader />
-      ) : !error ? (
-        <Message variant="danger">error</Message>
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
       ) : (
         <Table striped bordered hover responsive className="table-sm">
          <thead>
@@ -30,8 +30,44 @@ const OrderListScreen = () => {
             <th></th>
           </tr>
          </thead>
+         <tbody>
+          { orders.map((order)=>(
+            <tr key={order._id}>
+              <td>{order._id}</td>
+              <td>{order.user && order.user.name}</td>
+              <td>{order.createdAt.substring(0, 10)}</td>
+              <td>{order.totalprice}</td>
+              <td>
+              {order.isPaid ? (
+                order.paidAt.substring(0, 10)
+              ) : (
+                <FaTimes style={{ color: 'red'}} />
+
+              )}
+              </td>
+
+              <td>
+              {order.isDelivered ? (
+                order.deliveredAt.substring(0, 10)
+              ) : (
+                <FaTimes style={{ color: 'red'}} />
+
+              )}
+              </td>
+              <td>
+                <LinkContainer to={`/order/${order._id}`}>
+                  <Button variant='light' className="btn-sm">
+                    Details
+                  </Button>
+                </LinkContainer>
+
+              </td>
+            </tr>
+          ))}
+
+         </tbody>
         </Table>
-      )} */}
+      )} 
     
     </>
   );
