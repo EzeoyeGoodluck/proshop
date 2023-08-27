0660a88f6d7aca7ed5fe9395dd01e81f7d6a963a
Ezeoye Goodluck <Ezeoyeg@gmail.com>
2023-08-27 11:35:13 +0100

    commit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -77,7 +77,7 @@ const Header = () => {
                 <LinkContainer to='/admin/userlist'>
                   <NavDropdown.Item>Users</NavDropdown.Item>
                 </LinkContainer>
-                <LinkContainer to='/admin/productlist'>
+                <LinkContainer to='/admin/productlistscreen'>
                   <NavDropdown.Item>Products</NavDropdown.Item>
                 </LinkContainer>
               </NavDropdown>
