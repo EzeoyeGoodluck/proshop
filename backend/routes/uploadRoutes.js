79db287ba3a658394577ce6441894cd5ea478257
Ezeoye Goodluck <Ezeoyeg@gmail.com>
2023-08-30 12:36:03 +0100

    commit

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,6 +1,11 @@
 import path from 'path';
 import  express  from 'express';
 import multer from 'multer'
+
+
+
+
+
 const router = express.Router();
 
 
@@ -8,7 +13,7 @@ const storage = multer.diskStorage({
     destination(req, file, cb){
         cb(null, 'uploads/');
     },
-    filename( req, file, cd) {
+    filename( req, file, cb) {
         cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`);
     }
 });
