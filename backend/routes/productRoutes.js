4d8158cbde4a9eda792c90a5bd8677700f4c8a8e
Ezeoye Goodluck <Ezeoyeg@gmail.com>
2023-09-01 09:16:50 +0100

    commit

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -8,6 +8,7 @@ import { protect, admin} from '../middleware/authMiddleware.js'
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
+router.route('/:id/reviews').post(protect, createdProductReview);
 
 
 
