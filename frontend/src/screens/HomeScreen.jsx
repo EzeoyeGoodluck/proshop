45e5dcd54e326eb024d483aaf3eaa48d109019ab
Ezeoye Goodluck <Ezeoyeg@gmail.com>
2023-09-01 13:56:18 +0100

    commit

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,11 +1,15 @@
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
+import { useParams } from "react-router-dom";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 
 const HomeScreen = () => {
-  const { data, isLoading, error } = useGetProductsQuery();
+
+  const { pageNumber } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({ pageNumber});
+  
   return (
     <>
       {isLoading ? (
