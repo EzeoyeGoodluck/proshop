7fa48b49fb8deefed139d51c46d493e22c0466dc
Ezeoye Goodluck <Ezeoyeg@gmail.com>
2023-09-01 15:39:18 +0100

    commit

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -4,12 +4,13 @@ import { useParams } from "react-router-dom";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
+import Paginate from "../components/Paginate";
 
 const HomeScreen = () => {
 
   const { pageNumber } = useParams();
   const { data, isLoading, error } = useGetProductsQuery({ pageNumber});
-  
+
   return (
     <>
       {isLoading ? (
@@ -28,6 +29,9 @@ const HomeScreen = () => {
               </Col>
             ))}
           </Row>
+          <Paginate
+          pages={data.pages}
+          page={data.page}/>
         </>
       )}
     </>
