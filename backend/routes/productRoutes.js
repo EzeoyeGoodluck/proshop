2896dcfff1219cbdb6d2a2433ff908634e1fa45e
Ezeoye Goodluck <Ezeoyeg@gmail.com>
2023-09-02 12:10:51 +0100

    commit

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,15 +2,24 @@ import express from "express";
 const router = express.Router();
 // import asyncHandler from '../middleware/asyncHandler.js';
 // import Product from '../models/productModel.js';
-import {getProducts, getProductById, createProduct, updateProduct, deleteProduct, createdProductReview} from '../controllers/productController.js';
-import { protect, admin} from '../middleware/authMiddleware.js'
+import {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  createdProductReview,
+  getTopProducts,
+} from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
-
-router.route('/').get(getProducts).post(protect, admin, createProduct);
-router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
-router.route('/:id/reviews').post(protect, createdProductReview);
-
-
-
+router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.get('/top', getTopProducts);
+router
+  .route("/:id")
+  .get(getProductById)
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
+router.route("/:id/reviews").post(protect, createdProductReview);
 
 export default router;
