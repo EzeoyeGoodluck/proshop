67f77c5096f63391c959b7e437f2df8a6b946f6f
Ezeoye Goodluck <Ezeoyeg@gmail.com>
2023-09-02 12:05:32 +0100

    commit

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,45 +1,47 @@
-import asyncHandler from "../middleware/asyncHandler.js"
+import asyncHandler from "../middleware/asyncHandler.js";
 import Product from "../models/productModel.js";
-
 
 // @desc Fetch all proucts
 // @route GET /api/products
 // @access Public
 
 const getProducts = asyncHandler(async (req, res) => {
-    const pageSize = 8;
+  const pageSize = 8;
 
-    const page = Number(req.query.pageNumber) || 1;
+  const page = Number(req.query.pageNumber) || 1;
 
-    const keyword = req.query.keyword ? { name: { $regex: req.query.keyword, $options: 'i' }} : {};
+  const keyword = req.query.keyword
+    ? { name: { $regex: req.query.keyword, $options: "i" } }
+    : {};
 
+  const count = await Product.countDocuments({ ...keyword });
 
-
-
-    const count = await Product.countDocuments({ ...keyword});
-
-
-    const products = await Product.find({...keyword})
+  const products = await Product.find({ ...keyword })
     .limit(pageSize)
     .skip(pageSize * (page - 1));
-    res.json({products, page, pages: Math.ceil(count / pageSize) });
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
-
 
 // @desc Fetch a proucts
 // @route GET /api/products/:id
 // @access Public
 const getProductById = asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id);
 
-    if(product){
-    
+  if (product) {
     return res.json(product);
-}else {
+  } else {
     res.status(404);
-    throw new Error('Resource not found');
-}
+    throw new Error("Resource not found");
+  }
+});
 
+// @desc Get top rated products
+// @route GET /api/products/top
+// @access Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+  res.status(200).json(products);
 });
 
 // @desc create a product
@@ -47,36 +49,33 @@ const getProductById = asyncHandler(async (req, res) => {
 // @access Private/Admin
 
 const createProduct = asyncHandler(async (req, res) => {
-    const product = new Product({
-        name: 'Sample name',
-        price: 0,
-        user: req.user._id,
-        image: '/images/sample.jpg',
-        brand: 'Sample brand',
-        category: 'Sample category',
-        countInStock: 0,
-        numReviews: 0,
-        description: "Sample description",
-    })    
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "Sample brand",
+    category: "Sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
 
-
-    const createdProduct = await product.save();
-    res.status(201).json(createdProduct);
-  
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
 });
-
-
 
 // @desc Update  a prouct
 // @route PUT /api/products/:id
 // @access Private/admin
 
 const updateProduct = asyncHandler(async (req, res) => {
-   const {name, price, description, image, brand, category, countInStock } = req.body;
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
 
-   const product = await Product.findById(req.params.id);
+  const product = await Product.findById(req.params.id);
 
-   if(product){
+  if (product) {
     product.name = name;
     product.price = price;
     product.description = description;
@@ -86,12 +85,10 @@ const updateProduct = asyncHandler(async (req, res) => {
 
     const updatedProduct = await product.save();
     res.json(updatedProduct);
-   } else{
-
+  } else {
     res.status(404);
-    throw new Error('Resource not found');
-
-   }
+    throw new Error("Resource not found");
+  }
 });
 
 // @desc Delete  a prouct
@@ -99,67 +96,65 @@ const updateProduct = asyncHandler(async (req, res) => {
 // @access Private/admin
 
 const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
 
-    const product = await Product.findById(req.params.id);
+  if (product) {
+    await Product.deleteOne({ _id: product._id });
+    res.status(200).json({ message: "product deleted" });
+  } else {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+});
 
- 
-    if(product){
-        await Product.deleteOne({ _id: product._id});
-        res.status(200).json({message: 'product deleted' });
-    } else{
-     res.status(404);
-     throw new Error('Resource not found');
- 
-    }
- });
-
- // @desc Create  a review
+// @desc Create  a review
 // @route POST /api/products/:id/reviews
 // @access Private
 
 const createdProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
 
-    const { rating, comment } = req.body;
+  const product = await Product.findById(req.params.id);
 
-    const product = await Product.findById(req.params.id);
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (review) => review.user.toString() === req.user._id.toString()
+    );
 
- 
-    if(product){
-        const alreadyReviewed = product.reviews.find(
-            (review) => review.user.toString()  === req.user._id.toString()
-        )
-
-        if(alreadyReviewed) {
-            res.status(400);
-            throw new Error('Product already reviewed');
-        }
-
-        const review = {
-            name: req.user.name,
-            rating: Number(rating),
-            comment,
-            user: req.user._id,
-        };
-
-        product.reviews.push(review);
-
-        product.numReviews = product.reviews.length;
-
-        product.rating = 
-        product.reviews.reduce((acc, review) => acc + review.rating, 0 ) /
-        product.reviews.length; 
-
-        await product.save();
-
-        res.status(201).json({ message: 'Review added' });
-
-    } else{
-     res.status(404);
-     throw new Error('Resource not found');
- 
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error("Product already reviewed");
     }
- });
-  
- 
 
-export {getProducts, getProductById, createProduct, updateProduct, deleteProduct, createdProductReview};
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+
+    product.numReviews = product.reviews.length;
+
+    product.rating =
+      product.reviews.reduce((acc, review) => acc + review.rating, 0) /
+      product.reviews.length;
+
+    await product.save();
+
+    res.status(201).json({ message: "Review added" });
+  } else {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+});
+
+export {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  createdProductReview,
+};
